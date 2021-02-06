eb61fa19adf0a7c8b6eaf91535f6742a08903fa6
Liz Shigetoshi <lizshigetoshi@gmail.com>
2021-02-06 11:55:54 -0800

    :lipstick: Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import EmojiBar from '../components/EmojiBar';
 import Layout from '../components/Layout';
+import SectionHeading from '../components/SectionHeading';
 
 export default function NotFound() {
-  return <Layout>Oof</Layout>;
+  return (
+    <Layout>
+      <div className="initial-section" />
+      <div className="section end-wrapper">
+        <SectionHeading>Uh oh</SectionHeading>
+        <article className="section-content">
+          <EmojiBar emojis={['mag_right', 'question', 'weary']} />
+          <p>
+            This page doesn't exist.{' '}
+            <a href="https://lizshigetoshi.com">Let's go back home.</a>
+          </p>
+        </article>
+      </div>
+    </Layout>
+  );
 }
